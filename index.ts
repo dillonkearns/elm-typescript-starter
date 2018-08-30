d2930333bda4d31e865287b53e2aff012cab873a
Dillon Kearns <dillon@dillonkearns.com>
2018-08-30 14:23:15 -0700

    Update npm package versions.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as Elm from './src/Main'
-let app = Elm.Main.fullscreen()
-app.ports.hello.subscribe(name => console.log(`Hello ${name}!!`))
-app.ports.reply.send(12345)
+import * as Elm from "./src/Main";
+let app = Elm.Main.fullscreen();
+app.ports.hello.subscribe(name => console.log(`Hello ${name}!!`));
+app.ports.reply.send(12345);
