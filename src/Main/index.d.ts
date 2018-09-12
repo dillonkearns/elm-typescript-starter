d7654b7d40a6da73371ed52b1beb35f73e0e4f7b
Dillon Kearns <dillon@dillonkearns.com>
2018-09-12 11:53:07 -0700

    Update `elm-typescript-interop` version.

--- src/Main/index.d.ts
+++ src/Main/index.d.ts
@@ -4,17 +4,19 @@
 
 export namespace Elm {
   namespace Main {
-    interface App {
+    export interface App {
       ports: {
         hello: {
-          subscribe(callback: (data: string) => void): void;
-        };
+          subscribe(callback: (data: string) => void): void
+        }
         reply: {
-          send(data: number): void;
-        };
+          send(data: number): void
+        }
+        jsonPort: {
+          subscribe(callback: (data: unknown) => void): void
+        }
       };
     }
-
     export function init(options: {
       node?: HTMLElement | null;
       flags: null;
