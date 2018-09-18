4e03d446a7a71ad286171911c11066b9eada0881
Dillon Kearns <dillon@dillonkearns.com>
2018-09-17 18:23:48 -0700

    Remove some boilerplate.

--- index.ts
+++ index.ts
@@ -4,23 +4,4 @@ document.addEventListener("DOMContentLoaded", function() {
   let app = Elm.Main.init({
     flags: null
   });
-  console.log(app);
-
-  app.ports.storageStoreItem.subscribe(thing => {
-    console.log("setting to...", JSON.stringify(thing));
-    localStorage.setItem(thing.key, JSON.stringify(thing.value));
-  });
-
-  app.ports.lookItemUp.subscribe(key => {
-    console.log("@@@lookItemUp", key);
-    const lookupResult = localStorage.getItem(key);
-    if (lookupResult === null) {
-      app.ports.storageItemNotFound.send({ key: key });
-    } else {
-      app.ports.storageItemFound.send({
-        key: key,
-        value: JSON.parse(lookupResult)
-      });
-    }
-  });
 });
