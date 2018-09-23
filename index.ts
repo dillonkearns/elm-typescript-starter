e0210c3efdcbe22ea9a8b571696cb39853c9cba8
Dillon Kearns <dillon@dillonkearns.com>
2018-09-23 10:52:45 -0700

    Update starting point.

--- index.ts
+++ index.ts
@@ -1,34 +1,18 @@
 import { Elm } from "./src/Main";
 import * as moment from "moment";
 
-document.addEventListener("DOMContentLoaded", function() {
-  const app = Elm.Main.init({ flags: null });
-  function showClockReading() {
-    console.log(clockReading());
-  }
+const app = Elm.Main.init({ flags: null });
+function showClockReading() {
+  console.log(clockReading());
+}
 
-  /*
+function setLocale(newLocale: string) {
+  moment.locale(newLocale);
+  showClockReading();
+}
 
-    Boilerplate
+function clockReading() {
+  return moment().format("LL LTS");
+}
 
-
-
-
-
-
-
-
-
-
-  */
-  function setLocale(newLocale: string) {
-    moment.locale(newLocale);
-    showClockReading();
-  }
-
-  function clockReading() {
-    return moment().format("LL LTS");
-  }
-
-  setInterval(showClockReading, 1000);
-});
+setInterval(showClockReading, 1000);
